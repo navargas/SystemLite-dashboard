44de9d849575db91f6eb5dee026d23f2c31f4e2f
Nicholas Vargas <mail@nickvargas.net>
2015-12-20 17:42:07 -0800

    Add log side-bar

---
+++ js/logs.js
@@ -0,0 +1,34 @@
+var sampleData = [
+  {msg: "This is an example", level:'warn', ts:1450658798286}
+];
+
+var logger = new Vue({
+  el: '#logelement',
+  methods: {
+    hideLogs: function() {
+      this.isHidden = true;
+      console.log('show');
+    },
+    showLogs: function() {
+      this.isHidden = false;
+      this.stopAlert();
+    },
+    startAlert: function() {
+      var that = this;
+      this.alertInterval = setInterval(function() {
+        that.flashRed = !that.flashRed;
+      }, 1000);
+    },
+    stopAlert: function() {
+      if (this.alertInterval) {
+        clearInterval(this.alertInterval);
+      }
+      this.flashRed = false;
+    }
+  },
+  data: {
+    log: sampleData,
+    isHidden: true,
+    flashRed: false
+  }
+});

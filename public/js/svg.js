466036fce87cbb69a53e11d05d0b0378e18021b5
Nicholas Vargas <mail@nickvargas.net>
2015-12-21 23:05:15 -0800

    Refactor to use layerY instead of y-offset

--- public/js/svg.js
+++ public/js/svg.js
@@ -55,14 +55,10 @@ var svgCanvas = new Vue({
       this.scaleFactor += factor;
     },
     mouseMove: function(event) {
-      var topOffset = document
-                      .getElementById('svgCanvas')
-                      .getBoundingClientRect()
-                      .top;
-      var dx = event.x/this.scaleFactor - this.mouse.x;
-      var dy = event.y/this.scaleFactor - this.mouse.y - topOffset/this.scaleFactor;
-      this.mouse.x = event.x/this.scaleFactor;
-      this.mouse.y = event.y/this.scaleFactor - topOffset/this.scaleFactor;
+      var dx = event.layerX/this.scaleFactor - this.mouse.x;
+      var dy = event.layerY/this.scaleFactor - this.mouse.y;
+      this.mouse.x = event.layerX/this.scaleFactor;
+      this.mouse.y = event.layerY/this.scaleFactor;
       if (this.currentTarget !== undefined) {
         this.currentTarget.x += dx;
         this.currentTarget.y += dy;
